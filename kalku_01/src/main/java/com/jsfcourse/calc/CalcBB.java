cc2cb2aeeed8a5d3010bc5c4fe331ad0e68f930c
Linkoler <162218775+Linkoler@users.noreply.github.com>
2024-10-20 22:23:17 +0200

    Add files via upload

---
+++ kalku_01/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -0,0 +1,94 @@
+package com.jsfcourse.calc;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class CalcBB {
+	private String x;
+	private String y;
+	private String z;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	public String getZ() {
+		return z;
+	}
+
+	public void setZ(String z) {
+		this.z = z;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double x = Double.parseDouble(this.x); //kwota
+			double y = Double.parseDouble(this.y); //lata
+			double z = Double.parseDouble(this.z); //oproc
+
+			double mies = y * 12;
+			double proc = (z / 100) / 12;
+			result = (x * proc * Math.pow((1 + proc), mies))/(Math.pow((1 + proc), mies) - 1);
+			
+			result = (double) (Math.round(result * 100)/ 100.0);
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "miesięczna rata: " + result + "zł", null));
+		}
+		return null;
+	}
+
+	public String info() {
+		return "info";
+	}
+}
